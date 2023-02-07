8ac7ad6987187fe1a791b456296c3c34049fcbc3
Luis Jurgen Us <luisjurgenus@gmail.com>
2023-02-06 19:56:19 -0600

    commit doce

--- public/js/helpers/download-image.js
+++ public/js/helpers/download-image.js
@@ -1,9 +1,10 @@
 
 
 const downloadImg= ()=>{
-   const sreenshotTarget = document.querySelector('#container-thw');
-   const params=  { logging: true, letterRendering: 1, allowTaint: false,  useCORS: true } 
-   html2canvas (sreenshotTarget,params).then((canvas)=>{
+   let screenshotTarget = document.querySelector('#container-thw');
+   
+   const params=  {  width: screenshotTarget.offsetWidth - 0.25, logging: true, letterRendering: 1, allowTaint: false, scale:2, useCORS: true } 
+   html2canvas (screenshotTarget,params).then((canvas)=>{
       const base64image = canvas.toDataURL("image/png");
       let anchor = document.createElement('a');
       anchor.setAttribute('href',base64image);
